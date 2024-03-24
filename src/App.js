e83fc2d6f3ee3662e00d9c47681acf08198431f2
MethuParoi <paroimethu@gmail.com>
2024-03-24 11:48:09 +0530

    source link changed

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
           setLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
