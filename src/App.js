7c7af81f014fba8e24b04827a437c87bcbff617d
MethuParoi <paroimethu@gmail.com>
2024-07-16 11:36:21 +0530

    scrollbar added to movie list

--- src/App.js
+++ src/App.js
@@ -185,11 +185,17 @@ function Main({ children }) {
 
 function MovieList({ movies, onSelectMovie }) {
   return (
-    <ul className="list list-movies">
-      {movies?.map((movie) => (
-        <Movie movie={movie} onSelectMovie={onSelectMovie} key={movie.imdbID} />
-      ))}
-    </ul>
+    <div className="movie-list">
+      <ul className="list list-movies">
+        {movies?.map((movie) => (
+          <Movie
+            movie={movie}
+            onSelectMovie={onSelectMovie}
+            key={movie.imdbID}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
 
